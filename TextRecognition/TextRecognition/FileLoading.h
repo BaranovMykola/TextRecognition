d6e29f689215e08af5e224563b706430e356282a
BaranovMykola <mapsg32@gmail.com>
2018-02-28 19:31:42 +0200

    Added docs to file loading module

--- TextRecognition/TextRecognition/FileLoading.h
+++ TextRecognition/TextRecognition/FileLoading.h
@@ -2,4 +2,9 @@
 
 #include <opencv2\core.hpp>
 
+/**
+ * \brief Load images from file to the image
+ * \param img Image to load in
+ * \param fileName Name of file (without extension). Loaded 'TestSamplePathPrefix+fileName+".jpg"'
+ */
 void loadImg(cv::Mat & img, const std::string & fileName);
