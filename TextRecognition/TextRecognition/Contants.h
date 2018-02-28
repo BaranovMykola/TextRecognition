a36fa4aeaf5edb7e92d01fed5f9d1b83dc454b7f
BaranovMykola <mapsg32@gmail.com>
2018-02-28 19:23:41 +0200

    Added docs to constants

--- TextRecognition/TextRecognition/Contants.h
+++ TextRecognition/TextRecognition/Contants.h
@@ -1,10 +1,32 @@
 #pragma once
 #include <string>
 
-const std::string SamplePathPRefix = "../samples/";
-const std::string ClassifierPrefix = "../Classifier/";
-const std::string TextSamplePathPrefix = SamplePathPRefix + "letters/original/";
-const std::string TestSamplePathPrefix = SamplePathPRefix + "letters/test/";
-const std::string TextDatasetPathPrefix = SamplePathPRefix + "dataset/source/";
+/**
+ * \brief Path prefix to 'samples' folder
+ */
+const std::string SamplePathPrefix = "../samples/";
 
+/**
+ * \brief Path prefix to 'Classifier' fodler
+ */
+const std::string ClassifierPrefix = "../Classifier/";
+
+/**
+ * \brief Path to original images
+ */
+const std::string TextSamplePathPrefix = SamplePathPrefix + "letters/original/";
+
+/**
+ * \brief Path to test images
+ */
+const std::string TestSamplePathPrefix = SamplePathPrefix + "letters/test/";
+
+/**
+ * \brief Path to dataset source images
+ */
+const std::string TextDatasetPathPrefix = SamplePathPrefix + "dataset/source/";
+
+/**
+ * \brief Max resolution of images to skrew detection
+ */
 const float SkrewRestoringImageSize = 500;
